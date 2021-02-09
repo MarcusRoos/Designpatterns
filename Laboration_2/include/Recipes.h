6de0550f8ee53314e191a74e040eba3f8ef6735e
Marcus Roos <maro1904@student.miun.se>
2021-02-10 00:43:32 +0100

    Fixed recipes.h and moved the implementations to its own .cpp

--- Laboration_2/include/Recipes.h
+++ Laboration_2/include/Recipes.h
@@ -16,98 +16,36 @@ class Pizza : public BakingRecepy
 {
 
 public:
-    Pizza()
-    {
-        neededIng.emplace_back("oil");
-        neededIng.emplace_back("yeast");
-        neededIng.emplace_back("wheat-flour");
-        neededIng.emplace_back("salt");
-    }
-
+    Pizza();
     ~Pizza() override = default;
-    void bakeIt() override {
-        std::cout << "Baking Pizza: ";
-        for (const auto& ingExists : neededIng){
-            std::cout << ingExists.getName() << ", ";
-        }
-        std::cout << std::endl << "Mash ingredients together, put topping on "
-                                  "dough, keep in oven at "
-                                  "225C for 10min." << std::endl << std::endl;
-    }
+    void bakeIt() override;
 
 };
 
 class Scones : public BakingRecepy
 {
 public:
-    Scones()
-    {
-        neededIng.emplace_back("oil");
-        neededIng.emplace_back("baking-powder");
-        neededIng.emplace_back("wheat-flour");
-        neededIng.emplace_back("salt");
-        neededIng.emplace_back("sugar");
-        neededIng.emplace_back("milk");
-    }
+    Scones();
+
     ~Scones() override = default;
-    void bakeIt() override {
-        std::cout << "Baking Scones: ";
-        for (const auto& ingExists : neededIng){
-            std::cout << ingExists.getName() << ", ";
-        }
-        std::cout << std::endl << "Mash ingredients together, put dough in "
-                                  "oven at 225C for 10min."
-                << std::endl << std::endl;
-    }
+    void bakeIt() override;
 };
 class Sockerkaka : public BakingRecepy
 {
 
 public:
-    Sockerkaka()
-    {
-        neededIng.emplace_back("egg");
-        neededIng.emplace_back("sugar");
-        neededIng.emplace_back("baking-powder");
-        neededIng.emplace_back("wheat-flour");
-        neededIng.emplace_back("marge");
-        neededIng.emplace_back("lemon");
-    }
+    Sockerkaka();
     ~Sockerkaka() override = default;
-    void bakeIt() override {
-        std::cout << "Baking Sockerkaka: ";
-        for (const auto& ingExists : neededIng){
-            std::cout << ingExists.getName() << ", ";
-        }
-        std::cout << std::endl << "Mash ingredients together, put in oven "
-                                  "at 225C for 15min. " << std::endl
-                                  << std::endl;
-    }
+    void bakeIt() override;
 };
 
 class Kladdkaka : public BakingRecepy
 {
 public:
-    Kladdkaka() {
-        neededIng.emplace_back("egg");
-        neededIng.emplace_back("sugar");
-        neededIng.emplace_back("baking-powder");
-        neededIng.emplace_back("wheat-flour");
-        neededIng.emplace_back("salt");
-        neededIng.emplace_back("marge");
-        neededIng.emplace_back("cocoa-powder");
-    }
+    Kladdkaka();
 
     ~Kladdkaka() override = default;
-    void bakeIt() override {
-        std::cout << "Baking Kladdkaka: ";
-        for (const auto& ingExists : neededIng){
-            std::cout << ingExists.getName() << ", ";
-        }
-        std::cout << std::endl << "Mash ingredients together, "
-                                  "keep in oven at 200C for 10min."
-                << std::endl << std::endl;
-    }
+    void bakeIt() override;
 };
 
 #endif //DT063G_RECIPES_H
---
+++ Laboration_2/src/Recipes.cpp
@@ -0,0 +1,82 @@
+//
+// Created by Marcus Roos on 2021-02-10.
+// Mittuniversitet
+// StudentID: Maro1904
+//
+
+#include "Recipes.h"
+
+Pizza::Pizza() {
+        neededIng.emplace_back("oil");
+        neededIng.emplace_back("yeast");
+        neededIng.emplace_back("wheat-flour");
+        neededIng.emplace_back("salt");
+}
+
+void Pizza::bakeIt() {
+        std::cout << "Baking Pizza: ";
+        for (const auto& ingExists : neededIng){
+            std::cout << ingExists.getName() << ", ";
+        }
+        std::cout << std::endl << "Mash ingredients together, put topping on "
+                                  "dough, keep in oven at "
+                                  "225C for 10min." << std::endl << std::endl;
+}
+
+Scones::Scones() {
+    neededIng.emplace_back("oil");
+    neededIng.emplace_back("baking-powder");
+    neededIng.emplace_back("wheat-flour");
+    neededIng.emplace_back("salt");
+    neededIng.emplace_back("sugar");
+    neededIng.emplace_back("milk");
+}
+
+void Scones::bakeIt() {
+    std::cout << "Baking Scones: ";
+    for (const auto& ingExists : neededIng){
+        std::cout << ingExists.getName() << ", ";
+    }
+    std::cout << std::endl << "Mash ingredients together, put dough in "
+                              "oven at 225C for 10min."
+              << std::endl << std::endl;
+}
+
+Sockerkaka::Sockerkaka() {
+    neededIng.emplace_back("egg");
+    neededIng.emplace_back("sugar");
+    neededIng.emplace_back("baking-powder");
+    neededIng.emplace_back("wheat-flour");
+    neededIng.emplace_back("marge");
+    neededIng.emplace_back("lemon");
+}
+
+void Sockerkaka::bakeIt() {
+    std::cout << "Baking Sockerkaka: ";
+    for (const auto& ingExists : neededIng){
+        std::cout << ingExists.getName() << ", ";
+    }
+    std::cout << std::endl << "Mash ingredients together, put in oven "
+                              "at 225C for 15min. " << std::endl
+              << std::endl;
+}
+
+Kladdkaka::Kladdkaka() {
+    neededIng.emplace_back("egg");
+    neededIng.emplace_back("sugar");
+    neededIng.emplace_back("baking-powder");
+    neededIng.emplace_back("wheat-flour");
+    neededIng.emplace_back("salt");
+    neededIng.emplace_back("marge");
+    neededIng.emplace_back("cocoa-powder");
+}
+
+void Kladdkaka::bakeIt() {
+    std::cout << "Baking Kladdkaka: ";
+    for (const auto& ingExists : neededIng){
+        std::cout << ingExists.getName() << ", ";
+    }
+    std::cout << std::endl << "Mash ingredients together, "
+                              "keep in oven at 200C for 10min."
+              << std::endl << std::endl;
+}
